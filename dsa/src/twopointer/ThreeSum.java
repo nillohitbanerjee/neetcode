0a3b476c35304cb607cff452b884361362df4d22
nillohitbanerjee <nillohitbanerjee@gmail.com>
2024-12-02 02:04:00 +0530

    ThreeSum problem

---
+++ dsa/src/twopointer/ThreeSum.java
@@ -0,0 +1,48 @@
+package twopointer;
+
+import java.util.*;
+
+public class ThreeSum {
+
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> output = new HashSet<>();
+        int target =0;
+        Arrays.sort(nums);
+        for(int i=0 ; i<nums.length ; i++){
+            int start =0;
+            int end = nums.length-1;
+            int temp = target-nums[i];
+
+            while(start<end){
+
+                if(start ==i )
+                    start++;
+                if(end ==i)
+                    end--;
+
+                if(nums[start]+nums[end]>temp){
+                    end--;
+                }
+                else if(nums[start]+nums[end]<temp){
+                    start++;
+                }
+                else{
+                    List<Integer> tempList = new ArrayList<>();
+                    if(i!=start && i!=end && start!=end){
+                        tempList.add(nums[i]);
+                        tempList.add(nums[start]);
+                        tempList.add(nums[end]);
+                        Collections.sort(tempList);
+                        output.add(tempList);
+                        end--;
+                        start++;
+                    }
+                }
+            }
+        }
+        List<List<Integer>> list =new ArrayList<>(); //Creation of ArrayList
+        list.addAll(output);
+        return list;
+    }
+}
