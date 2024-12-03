67f9a1f721cd8a8d05bc28682937f680e5f8d661
nillohitbanerjee <nillohitbanerjee@gmail.com>
2024-12-04 00:54:15 +0530

    trapping-rain-water problem

--- dsa/src/twopointer/TrappingRainWater.java
+++ dsa/src/twopointer/TrappingRainWater.java
@@ -11,32 +11,20 @@ public class TrappingRainWater {
         right[height.length-1]= height[height.length-1];
         left[0]=height[0];
         for(int i= height.length-2;i>=0;i--){
-            if(height[i]>right[i+1]){
-                right[i]=height[i];
-            }
-            else
-                right[i]=right[i+1];
+            right[i] = Math.max(height[i], right[i + 1]);
         }
 
         for(int i= 1;i<height.length;i++){
-            if(height[i]>left[i-1]){
-                left[i]=height[i];
-            }
-            else
-                left[i]=left[i-1];
+            left[i] = Math.max(height[i], left[i - 1]);
         }
 
         for(int i=0; i<height.length ; i++){
             int temp =  Math.min(left[i],right[i])-height[i];
-            if(temp>0){
-                height[i]=temp;
-            }
-            else
-                height[i]=0;
+            height[i] = Math.max(temp, 0);
         }
         int op = 0 ;
-        for(int i=0; i<height.length ; i++){
-            op = op+height[i];
+        for (int j : height) {
+            op = op + j;
         }
         return op;
     }
